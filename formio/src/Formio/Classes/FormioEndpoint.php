303803e8d54410eb2670a0dbe8160b5cff063342
Barry Brands <barrybrands02@hotmail.com>
2022-04-20 09:30:19 +0200

    FormioEndpoint fix

--- formio/src/Formio/Classes/FormioEndpoint.php
+++ formio/src/Formio/Classes/FormioEndpoint.php
@@ -6,41 +6,41 @@ use OWC\Formio\Foundation\Plugin;
 
 class FormioEndpoint
 {
-  /** @var Plugin */
-  protected $plugin;
+    /** @var Plugin */
+    protected $plugin;
 
-  public function __construct(Plugin $plugin)
-  {
-    $this->plugin = $plugin;
-    $this->load_hooks();
-  }
-
-  private function load_hooks(): void
-  {
-    add_action('rest_api_init', function () {
-      register_rest_route('conductionnl/v1', '/author/(?P<id>\d+)', array(
-        'methods' => 'GET',
-        'callback' => 'my_awesome_func',
-      ));
-    });
-  }
-
-  /**
-   * Grab latest post title by an author!
-   *
-   * @param array $data Options for the function.
-   * @return string|null Post title for the latest,  * or null if none.
-   */
-  public function my_awesome_func($data)
-  {
-    $posts = get_posts(array(
-      'author' => $data['id'],
-    ));
-
-    if (empty($posts)) {
-      return null;
+    public function __construct(Plugin $plugin)
+    {
+        $this->plugin = $plugin;
+        $this->load_hooks();
     }
 
-    return $posts[0]->post_title;
-  }
+    private function load_hooks(): void
+    {
+        add_action('rest_api_init', function () {
+            register_rest_route('conductionnl/v1', '/author/(?P<id>\d+)', array(
+                'methods' => 'GET',
+                'callback' => [$this, 'my_awesome_func'],
+            ));
+        });
+    }
+
+    /**
+     * Grab latest post title by an author!
+     *
+     * @param array $data Options for the function.
+     * @return string|null Post title for the latest, * or null if none.
+     */
+    public function my_awesome_func($data)
+    {
+        $posts = get_posts(array(
+            'author' => $data['id'],
+        ));
+
+        if (empty($posts)) {
+            return null;
+        }
+
+        return $posts[0]->post_title;
+    }
 }
